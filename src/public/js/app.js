2513c9fdd588016cc6e42f8469fa1d6c8b56478f
root <root@guest.guest>
2021-04-17 03:15:30 +0000

    Add set name function

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,32 +1,50 @@
-document.getElementById("my-account").innerHTML = window.tronWeb.defaultAddress.base58;
+init();
 
-window.tronWeb.contract().at("TDKDBN394c4QShXpn8A74UcB4HHWXRx3hk").then(result => {
-    init(result);
-  });
+function init() {
 
-  function init(contract){
+    if (!window.tronWeb) return;
 
-    contract.setName("Yonatan").send().then(res => {
+    window.tronWeb.on("addressChanged", (res) => {
+        console.log(">>>>>>>>> Address changed", res);
+        //console.log(">>>>>>>>> Address changed",new Date());
+        if (window.tronWeb.ready && window.address === window.tronWeb.defaultAddress.base58) return;
+        //alert(">>>>>>>>> Reload page",new Date());
+
+        window.location.reload();
+
+    });
+
+    if (window.tronWeb.ready) {
+        //console.log(">>>>>>>>>> Ready",new Date());
+        window.address = window.tronWeb.defaultAddress.base58;
+        document.getElementById("my-account").innerHTML = window.address;
+
+        window.tronWeb.contract().at("TDKDBN394c4QShXpn8A74UcB4HHWXRx3hk").then(contract => {
+            window.contract = contract;
+            contract.greetings().call().then(res => {
+                console.log(res);
+                document.getElementById("greetings").innerHTML = res;
+            });
+
+     
+            
+
+        });
+    }
+
+}
+
+function setName(){
+    if(!document.querySelector("#name").value) return;
+    window.contract.setName(document.querySelector("#name").value).send().then(res => {
         console.log(res);
         alert("Name sent successfully");
-
         contract.greetings().call().then( res => {
             console.log(res);
-            //document.getElementById("greetings").innerHTML = contract.greetings();
+            document.getElementById("greetings").innerHTML = res;
         });
-
     }).catch( err => {
         console.log(err);
         alert("Error tryinig set name");
     });
-
-
-
-    
-
   }
-
-
-
-
-
